fa1c10eb0d81426bbc1a4c068b95851571370f6c
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2021-12-30 12:58:16 -0600

    add the funtion to display the Tasks

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const main = async() => {
                 tareas.crearTarea( descripcion );
                 break;
             case '2':
-                console.log( tareas._listado );
+                console.log( tareas.listadoTareas );
                 break;
         }
 
--- models/tareas.js
+++ models/tareas.js
@@ -7,6 +7,16 @@ class Tareas {
         this._listado = {};
     }
 
+    get listadoTareas() {
+        const listado = [];
+        Object.keys( this._listado ).forEach( key => {
+            const tarea = this._listado[key];
+            listado.push( tarea );
+        });
+
+        return listado;
+    }
+
     crearTarea( descripcion ) {
         const tarea = new Tarea( descripcion );
         this._listado[ tarea.id ] = tarea;
